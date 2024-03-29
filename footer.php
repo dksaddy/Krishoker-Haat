68affeab4ee13a5329e5e52d2b1817619cd59267
SM Maruph <smmaruph.bhbd2001@gmail.com>
2024-03-30 02:12:48 +0600

    footer updated

--- footer.php
+++ footer.php
@@ -1,47 +1,70 @@
-
 <section class="footer">
-      <div class="footer-row">
-        <div class="footer-col">
-          <h4>Info</h4>
-          <ul class="links">
-            <li><a href="http://localhost/project/aboutus.php">About Us</a></li>
-            <li><a href="#">Terms & Condition</a></li>
-            <li><a href="#">Privacy policy</a></li>
-            <li><a href="#">Contact us</a></li>
-            <li><a href="#">developer contact</a></li>
-          </ul>
-        </div>
-        <div class="footer-col">
-          <h4>Explore</h4>
-          <ul class="links">
-            <li><a href="http://localhost/project/aboutus.php">Home</a></li>
-            <li><a href="#">Top Restaurants</a></li>
-            <li><a href="#">Popular Now</a></li>
-            <li><a href="#">Hot deals</a></li>
-            <li><a href="#">Discount</a></li>
-          </ul>
-        </div>
-        <div class="footer-col">
-          <h4>Legal</h4>
-          <ul class="links">
-            <li><a href="#">Customer Agreement</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Security</a></li>
-          </ul>
-        </div>
-        <div class="footer-col">
-          <div class="icons">
-            <i class="fa-brands fa-facebook-f"></i>
-            <i class="fa-brands fa-twitter"></i>
-            <i class="fa-brands fa-linkedin"></i>
-            <i class="fa-brands fa-github"></i>
-          </div>
-          <p>
-            @Copyright Krishoker Haat 2024 All rights reserved.
-          </p>
-          <p>Developed by Team AK</p>
-        </div>
+  <div class="footer-row">
+    <div class="footer-col">
+      <h2>কৃষকের<span> হাট</span></h2>
+      <p>কৃষক এবং ভোক্তাদের সংযোগকারী বিশ্বস্ত প্ল্যাটফর্ম,
+               কৃষকের হাটে স্বাগতম৷আমরা কৃষকদের কাছ থেকে সরাসরি আপনার টেবিলে সাশ্রয়ী মূল্যের,উচ্চ মানের কৃষি পণ্য অফার করি৷ মধ্যস্বত্বভোগীদের নির্মূল করে আমরা স্বচ্ছতা, নির্ভরযোগ্যতা
+                 এবং ন্যায্য মূল্য নিশ্চিত করি৷ প্রত্যক্ষ সম্পর্ক গড়ে তোলার প্রতি আমাদের প্রতিশ্রুতি স্থানীয় সম্প্রদায়কে শক্তিশালী করে এবং টেকসই চাষাবাদের অনুশীলনকে উৎসাহিত করে ৷তাজা ফল এবং সবজি থেকে শস্য এবং আরও অনেক কিছু ৷</p>
+    </div>
+    <div class="footer-col">
+      <h5>আমাদেরকে অনুসরণ করুন</h5>
+      <div class="icons">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-linkedin"></i></a>
+        <a href="#"><i class="fab fa-github"></i></a>
       </div>
-    </section>
 
-</body>
+      <div class="subscribe">
+      <h5>অফার সম্পর্কে সর্বশেষ আপডেট পেতে, কুপন সম্পর্কে জানতে আমাদের সাবস্ক্রাইব করুন ৷</h5>
+        <input type="text" placeholder="&#x270e; ই-মেইল ​​ঠিকানা লিখুন " name="restaurantName">
+        <button type="submit">সাবস্ক্রাইব</button>
+      <h5>আমাদের সাথে যোগাযোগ করুন : </h5>
+      <p>ইমেইল: krishokerhat@gmail.com</p>
+      <p>ফোন:  +880 1321098082</p>
+      <p>ঠিকানা: নতুন বাজার, বাড্ডা, ঢাকা ৷</p>
+
+    </div>
+</div>
+  </div>
+  <div class="footer-row">
+    <div class="footer-col">
+        <h4>Info</h4>
+        <ul class="links">
+          <li><a href="#">হোম</a></li>
+          <li><a href="#">আমাদের সম্পর্কে</a></li>
+          <li><a href="#">যোগাযোগ</a></li>
+          <li><a href="#">Developer contact</a></li>
+          <div class="icons">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-linkedin"></i></a>
+              <a href="#"><i class="fab fa-github"></i></a>
+            </div>
+        </ul>
+      </div>
+      <div class="footer-col">
+        <h4>Explore</h4>
+        <ul class="links">
+          <li><a href="#">পণ্য</a></li>
+          <li><a href="#">শীর্ষ কৃষক</a></li>
+          <li><a href="#">জনপ্রিয়</a></li>
+          <li><a href="#">হট ডিল</a></li>
+          <li><a href="#">ডিসকাউন্ট</a></li>
+        </ul>
+      </div>
+      <div class="footer-col">
+        <h4>Legal</h4>
+        <ul class="links">
+          <li><a href="#">শর্তাবলী</a></li>
+          <li><a href="#">ফেরত সংক্রান্ত নীতি</a></li>
+          <li><a href="#">নিরাপত্তা</a></li>
+        </ul>
+      </div>
+  </div>
+  <div class="copyright">
+        <p>কৃষকের হাট 2024-এর সমস্ত অধিকার সংরক্ষিত</p>
+        <p>Developed by Team AK</p>
+  </div>
+</section>
+</div>

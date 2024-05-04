b69cd726c908b54f549e71d7d8e624ef85a09f00
ShakilAhmed201 <zh33663@gmail.com>
2024-05-04 12:05:46 +0600

    full Article added

--- Createpost.php
+++ Createpost.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/article.css">
+    <link rel="stylesheet" href="css\footer.css">
     <title>Article</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -34,14 +35,15 @@
 </div>
   <div class="col-md-4"></div>
   <div class="col-md-4"></div>
-  <div class="input-group mb-3 col-md-4">
+  <div class=" mb-3 col-md-4">
   <label class="input-group-text" for="inputGroupFile01">Upload Image</label>
   <input type="file" class="form-control" name="image" id="inputGroupFile01">
 </div>
 
 <div class="col-md-4"></div>
+<div class="col-md-4"></div>
  
-  <div class="col-md-4 d-flex align-items-center justify-content-center">
+  <div class="col-md-4 mb-5 d-flex align-items-center justify-content-center">
     <button type="submit" class="btn btn-success w-100">Post</button>
   </div>
 </form>
@@ -52,5 +54,6 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <?php include('footer.php')?>
 </body>
 </html>
